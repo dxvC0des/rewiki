e76cf4008490ab807f29f46afb86661f06c2f3f6
dxvC0des <dxv@dxvprojects.org>
2025-07-28 22:47:27 -0400

    Removed "Edit This Page" links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,8 +49,6 @@ deploymentBranch: 'gh-pages',
           sidebarPath: './sidebars.js',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/dxvC0des/rewiki',
           routeBasePath: '/', // Set this value to '/'.
         },
         blog: false
